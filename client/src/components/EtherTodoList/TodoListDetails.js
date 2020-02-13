3dcfd1f547a98fecb523049f36674a20d882138d
Felix Faerber <ffaerber@gmail.com>
2020-02-13 15:21:25 +0100

    load detail page

--- client/src/components/EtherTodoList/TodoListDetails.js
+++ client/src/components/EtherTodoList/TodoListDetails.js
@@ -1,13 +1,25 @@
 import React, { useContext, useEffect, useState } from 'react'
-
-import {
-  useParams
-} from "react-router-dom";
+import { MyContext} from '../../store/store'
+import { useParams } from "react-router-dom";
 
 export default function TodoListDetails() {
   const { listId } = useParams();
+  const [list, setList] = useState({});
+  const { state, actions } = useContext(MyContext)
+
+  useEffect(() => {
+    actions.callList(listId)
+  }, [listId]);
+
+  useEffect(() => {
+    const stateList = state.lists.find(l => l.id === listId )
+    setList(stateList ? {...list, ...stateList} : {})
+  }, [state.lists]);
 
   return (
-    <h3> Details for TodoList: {listId}</h3>
+    <div>
+      <h3> ID: {list.id}</h3>
+      <h3> Details for TodoList: {list.name}</h3>
+    </div>
   )
 }
