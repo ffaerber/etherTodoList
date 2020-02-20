ee9ecfc664326d11a4ec471a1d9271540398aeb3
Felix Faerber <ffaerber@gmail.com>
2020-02-20 13:38:54 +0100

    linting

--- test/etherTodoList.js
+++ test/etherTodoList.js
@@ -31,52 +31,42 @@ describe('etherTodoList', function() {
   it('should be able to create and update list as listowner', async () => {
     await etherTodoList.createList('myList1', { from: user1 });
     await etherTodoList.createList('myList2', { from: user1 });
-    const totalLists = await etherTodoList.totalLists();
+    const totalLists = await etherTodoList.totalLists()
     expect(totalLists).to.bignumber.equal(new BN(2));
-    const listIds = await Promise.all(_.times(totalLists, i => etherTodoList.listIds(i)));
-    expect(await etherTodoList.getList(listIds[0]))
-      .to.have.property('name')
-      .with.equal('myList1');
-    expect(await etherTodoList.getList(listIds[1]))
-      .to.have.property('name')
-      .with.equal('myList2');
+    const listIds = await Promise.all(_.times(totalLists, i => etherTodoList.listIds(i)))
+    expect(await etherTodoList.getList(listIds[0])).to.have.property('name').with.equal('myList1');
+    expect(await etherTodoList.getList(listIds[1])).to.have.property('name').with.equal('myList2');
     await etherTodoList.updateList(listIds[0], 'myNewList1', { from: user1 });
-    expect(await etherTodoList.getList(listIds[0]))
-      .to.have.property('name')
-      .with.equal('myNewList1');
+    expect(await etherTodoList.getList(listIds[0])).to.have.property('name').with.equal('myNewList1');
   });
+
 
   it('should be able to create and update todo as listowner', async () => {
     await etherTodoList.createList('myList1', { from: user1 });
-    const totalLists = await etherTodoList.totalLists();
+    const totalLists = await etherTodoList.totalLists()
     expect(totalLists).to.bignumber.equal(new BN(1));
-    const listIds = await Promise.all(_.times(totalLists, i => etherTodoList.listIds(i)));
-    expect(await etherTodoList.getList(listIds[0]))
-      .to.have.property('totalTodos')
-      .to.bignumber.equal(new BN(0));
+    const listIds = await Promise.all(_.times(totalLists, i => etherTodoList.listIds(i)))
+    expect(await etherTodoList.getList(listIds[0])).to.have.property('totalTodos').to.bignumber.equal(new BN(0));
 
     const msg = 'hello this is a todo!';
     await etherTodoList.createTodo(listIds[0], msg, { from: user1 });
-    expect(await etherTodoList.getTodo(listIds[0], 0))
-      .to.have.property('body')
-      .to.equal(msg);
+    expect(await etherTodoList.getTodo(listIds[0], 0)).to.have.property('body').to.equal(msg);
 
     const newMsg = 'hello this is a updated todo!';
-    await etherTodoList.updateTodo(listIds[0], 0, newMsg, { from: user1 });
-    expect(await etherTodoList.getTodo(listIds[0], 0))
-      .to.have.property('body')
-      .to.equal(newMsg);
+    await etherTodoList.updateTodo(listIds[0], 0, newMsg, { from: user1 })
+    expect(await etherTodoList.getTodo(listIds[0], 0)).to.have.property('body').to.equal(newMsg);
+
   });
+
 
   it('should not be able to update todo as foreigner', async () => {
     await etherTodoList.createList('myList1', { from: user2 });
-    const totalLists = await etherTodoList.totalLists();
-    const listIds = await Promise.all(_.times(totalLists, i => etherTodoList.listIds(i)));
+    const totalLists = await etherTodoList.totalLists()
+    const listIds = await Promise.all(_.times(totalLists, i => etherTodoList.listIds(i)))
     const msg = 'hello this is a todo!';
     await etherTodoList.createTodo(listIds[0], msg, { from: user1 });
-    await expectRevert(
-      etherTodoList.updateTodo(listIds[0], 0, msg, { from: user3 }),
-      'you are not allowed to do that.',
-    );
+    await expectRevert(etherTodoList.updateTodo(listIds[0], 0, msg, { from: user3 }), 'you are not allowed to do that.');
   });
+
+
 });
